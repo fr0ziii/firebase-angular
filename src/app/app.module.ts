9da3970883f30cfb554d8591e6b9e74b3e3ecf25
fr0ziii <fr0ziisc2@gmail.com>
2022-05-11 18:51:56 +0100

    pre-release [needs change style and format]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { EditBookComponent } from './components/edit-book/edit-book.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularMaterialModule } from './material.module';
+
+/* Firebase */
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
+
+import { BookService } from './shared/book.service';
+
+/* Reactive form services in Angular */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    EditBookComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [BookService],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCxY5w-Ko0HxPehTLEUdKf2bTTemHMokJA",
+    authDomain: "daw2-angular.firebaseapp.com",
+    databaseURL: "https://daw2-angular-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "daw2-angular",
+    storageBucket: "daw2-angular.appspot.com",
+    messagingSenderId: "308273014580",
+    appId: "1:308273014580:web:c6ad82d790f081c03ea761"
+  }
 };
 
 /*
