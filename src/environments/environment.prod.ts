849d5b4d5bea180464a4e75fb666de07fa1d4cc0
fr0ziii <fr0ziisc2@gmail.com>
2022-05-22 19:32:06 +0100

    deploy fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCxY5w-Ko0HxPehTLEUdKf2bTTemHMokJA",
+    authDomain: "daw2-angular.firebaseapp.com",
+    databaseURL: "https://daw2-angular-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "daw2-angular",
+    storageBucket: "daw2-angular.appspot.com",
+    messagingSenderId: "308273014580",
+    appId: "1:308273014580:web:c6ad82d790f081c03ea761"
+  }
 };
